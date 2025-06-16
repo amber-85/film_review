2a6108a04f8372276cfbaaca2b17a1089d7f9e66
amber-85 <amber.an.bo@gmail.com>
2025-06-16 15:20:19 +0200

    update review.js with createAt

--- models/Review.js
+++ models/Review.js
@@ -5,6 +5,7 @@ const reviewSchema= new mongoose.Schema({
     userId: {type:mongoose.Schema.Types.ObjectId, ref:"User", required:true},
     rating: {type:Number, required:true, min:1, max:5},
     comment: {type:String, required:true},
+    createdAt: {type:Date, default:Date.now}
 });
 
 // export reviewSchema as a model
